3dffb31d35dd7e64d5a57dd39bea710abbddd199
Elijah J. <dead.knuckle@gmail.com>
2024-03-15 14:53:38 -0400

    Added quizlet_match to repo

---
+++ USERSCRIPT/quizlet_match.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Quizlet Match
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-15
+// @description  Quizlet Match Solver
+// @author       EJ
+// @match        https://quizlet.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=quizlet.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+})();
